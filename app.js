8ac7e56a1ff977aa7ecbf8f3c1ca709bd61e9357
SethEvry <92412142+SethEvry@users.noreply.github.com>
2021-11-19 20:39:29 -0500

    ye

--- app.js
+++ app.js
@@ -1,51 +1,63 @@
-const express = require('express');
-const { projects } = require('./data.json');
+const express = require("express");
+const { projects } = require("./data.json");
 const app = express();
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
-app.use('/static', express.static('public'));
+app.use("/static", express.static("public"));
 
-app.get('/', (req, res) =>{
-    res.render('index', { projects });
-})
+app.get("/", (req, res) => {
+	res.render("index", {
+		projects,
+	});
+});
 
 // error test route
 //
-// app.get('/error', (req, res, next) => {
-//     const err = new Error('Server error!')
-//     err.status = 500;
-//     next(err);
-// })
-
-app.get('/about', (req, res) => {
-    res.render('about');
+app.get('/error', (req, res, next) => {
+    const err = new Error('Server error!')
+    err.status = 500;
+    next(err);
 })
 
-app.get('/projects/:id', (req, res) => {
-    const project = projects.find(project => project.id === req.params.id)
-    if(project) {
-        res.render('project', { project })
-    } else {
-        res.status(404).send("404 Page not Found!");
-    }
-})
+app.get("/about", (req, res) => {
+	res.render("about");
+});
+
+app.get("/projects/:id", (req, res, next) => {
+	const project = projects.find((project) => project.id === req.params.id);
+	if (project) {
+		res.render("project", {
+			project,
+		});
+	} else {
+		const err = new Error();
+		err.status = 404;
+		err.message = "Sorry, that project does not exist.";
+		next(err);
+	}
+});
 
 //404 error handler
-app.use((req, res) => {
-    res.status(404).send("404 Page not Found!");
-
-} )
+app.use((req, res, next) => {
+	console.log("404 error handler engaged");
+    res.status(404).render("page-not-found")
+});
 
 // General error handler
 app.use((err, req, res, next) => {
-    const status = err.status ? err.status : 500;
-    const message = err.message ? err.message : "Something went wrong!";
-    res.status(status)
-    res.send(`${status}: ${message}`)
-})
-
+	if (err.status === 404) {
+		res.status(404).render("page-not-found", { err });
+	} else {
+		err.status = err.status ? err.status : 500;
+		err.message = err.message ? err.message : "Something went wrong!";
+		res.status(err.status);
+		res.render("error", {
+			err,
+		});
+	}
+});
 
 app.listen(3000, () => {
-    console.log("App has Started!")
-})
+	console.log("App has Started!");
+});
