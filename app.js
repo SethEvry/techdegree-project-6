6d0e049fa9c783040d1b823e2b1ef9b456563ffe
SethEvry <92412142+SethEvry@users.noreply.github.com>
2021-11-19 19:36:11 -0500

    do stuff and maybe things

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
 app.get('/', (req, res) =>{
-    res.render('index', projects);
+    res.render('index', { projects });
 })
 
 // error test route
@@ -25,16 +25,19 @@ app.get('/about', (req, res) => {
 app.get('/projects/:id', (req, res) => {
     const project = projects.find(project => project.id === req.params.id)
     if(project) {
-    res.render('project', project)
+        res.render('project', { project })
     } else {
-        res.redirect('/');
+        res.status(404).send("404 Page not Found!");
     }
 })
-app.use((req, res, next) => {
-    const err = new Error('Page does not exist!')
-    err.status = 404;
-    next(err);
+
+//404 error handler
+app.use((req, res) => {
+    res.status(404).send("404 Page not Found!");
+
 } )
+
+// General error handler
 app.use((err, req, res, next) => {
     const status = err.status ? err.status : 500;
     const message = err.message ? err.message : "Something went wrong!";
